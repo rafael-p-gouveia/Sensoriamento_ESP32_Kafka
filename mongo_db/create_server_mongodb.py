1bd5aeca7be80174fa8e54145fd67f2c4bda337f
rafael-p-gouveia <rafael.p.gouveia2@gmail.com>
2023-06-24 23:46:21 -0300

    Adding comments on mongo related code

--- mongo_db/create_server_mongodb.py
+++ mongo_db/create_server_mongodb.py
@@ -1,6 +1,7 @@
 import pymongo
 import sys
 
+#Print if database with expecified name exist
 def db_exists(client,db_name):
     dblist = client.list_database_names()
     if db_name in dblist:
@@ -10,6 +11,7 @@ def db_exists(client,db_name):
         print("The database '{0}' doesn't exists".format(db_name))
         return False
 
+#Print if column with expecified name exist
 def col_exists(db,col_name):
     collist = db.list_collection_names()
     if col_name in collist:
@@ -18,10 +20,9 @@ def col_exists(db,col_name):
     else:
         print("The collection '{0}' doesn't exists.".format(col_name))
         return False
-        
-    
-print("Hello, World!")
 
+
+#Attemp to connect to MongoDB application
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 try:
     myclient.admin.command('ping')
@@ -29,17 +30,22 @@ try:
 except:
     sys.exit("Connection failed to be established")
 
-mydb = myclient["SensorData"] #not truly created until it gets a collection
+#If succeded, selects the desired database
+mydb = myclient["SensorData"] #Not truly created until it gets a collection
 
+#Checks if the database existed or not and print the conclusion
 db_exists(myclient,"SensorData")
 
+#Selects the desired column
 mycol = mydb["data"] #not truly created until it gets content
 
+#If they do not already exist, insert dummy data
 if(not col_exists(mydb,"data")):
     print("Inserting dummy data in order to ensure database creation")
     mydict = {"_id": "0000-01-01 00:00:00.000000", "Temp": -255, "Umid": -1}
     mycol.insert_one(mydict)
     
+    #Test if column was successfully created
     db_exists(myclient,"SensorData")
     assert col_exists(mydb,"data"), "Database and/or collection failed to be created"
 
